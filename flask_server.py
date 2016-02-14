044ae94b97d668cc40ac08211f59c4b3cb6c6822
Janne Lehikoinen <janne.lehikoinen@tieto.com>
2016-02-14 08:36:34 +0200

    Tabs to soft tabs

--- flask_server.py
+++ flask_server.py
@@ -13,24 +13,24 @@ app = Flask(__name__)
 # Index / search
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	error = None
-	
-	if request.method == 'GET':
-		data = None
-	if request.method == 'POST':
-		search_input = request.form['search']
-		data = itunes_api.search_album(search_input, 'album', 100)
-		if not data:
-			error = 'No search results for ' + search_input
-	return render_template('index.html', data=data, error=error)
+  error = None
+  
+  if request.method == 'GET':
+    data = None
+  if request.method == 'POST':
+    search_input = request.form['search']
+    data = itunes_api.search_album(search_input, 'album', 100)
+    if not data:
+      error = 'No search results for ' + search_input
+  return render_template('index.html', data=data, error=error)
 
 # Info
 @app.route('/info')
 def show_info():
-	info = device_info.all_info()
-	return render_template('info.html', info=info)
+  info = device_info.all_info()
+  return render_template('info.html', info=info)
 
 # Launch server
 if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 5000))
-	app.run(host='0.0.0.0', port=port)
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port)
--- itunes_api.py
+++ itunes_api.py
@@ -1,5 +1,6 @@
 """
-# https://www.apple.com/itunes/affiliates/resources/documentation/itunes-store-web-service-search-api.html#searchexamples
+iTunes Search API:
+https://www.apple.com/itunes/affiliates/resources/documentation/itunes-store-web-service-search-api.html
 """
 
 import json
@@ -15,47 +16,51 @@ COUNTRY_CODE = 'fi'
 ###
 
 def search_album(query, entity, limit):
-	
-	main_array = []
 
-	base_url = 'https://itunes.apple.com/' + COUNTRY_CODE + '/search?term='
-	url = base_url + query + '&entity=' + entity + '&limit=' + str(limit)
+  main_array = []
 
-	try:
-		response = requests.get(url)
-		data_dict = response.json()
-	except Exception, e:
-		print str(e)
-	
-	# Quick and dirty iteration
-	for item in data_dict["results"]:
-		if (item["artistName"].lower()) == query.lower():
-			if item.get("artistName", None):
-				artist_name = item["artistName"]
-			if item.get("collectionName", None): 
-				album_name = item["collectionName"]
-			if item.get("collectionPrice", None):
-				album_price = item["collectionPrice"]
-			else:
-				album_price = "n/a"
-			if item.get("artworkUrl60", None):
-				album_cover = item["artworkUrl60"]
-			if item.get("releaseDate", None):
-				release_date = item["releaseDate"]
+  base_url = 'https://itunes.apple.com/' + COUNTRY_CODE + '/search?term='
+  url = base_url + query + '&entity=' + entity + '&limit=' + str(limit)
 
-			sub_array = [artist_name, album_name, album_price, album_cover, release_date]
-			main_array.append(sub_array)
-	
-	# Sort by releaseDate, latest first
-	main_array = sorted(main_array, key=itemgetter(4), reverse=True)
+  try:
+    response = requests.get(url)
+    data_dict = response.json()
+  except Exception, e:
+    print str(e)
 
-	return main_array
+  # Quick and dirty iteration
+  for item in data_dict["results"]:
+    if (item["artistName"].lower()) == query.lower():
+      if item.get("artistName", None):
+        artist_name = item["artistName"]
+      if item.get("collectionName", None): 
+        album_name = item["collectionName"]
+      if item.get("collectionPrice", None):
+        album_price = item["collectionPrice"]
+      else:
+        album_price = "n/a"
+      if item.get("artworkUrl60", None):
+        album_cover = item["artworkUrl60"]
+      if item.get("releaseDate", None):
+        release_date = item["releaseDate"]
+
+      sub_array = [artist_name, 
+                   album_name, 
+                   album_price, 
+                   album_cover, 
+                   release_date]
+      main_array.append(sub_array)
+
+  # Sort by releaseDate, latest first
+  main_array = sorted(main_array, key=itemgetter(4), reverse=True)
+
+  return main_array
 
 
 def	main():
-	
-	data = search_album('Anthrax', 'album', 100)
-	print data
-	
+
+  data = search_album('Anthrax', 'album', 100)
+  print data
+
 if __name__ == '__main__':
-	main()
+  main()
